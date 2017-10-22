4aec4da16e4de9732dd7a2321c607c7ceb17f2dd
krasi070 <krasi0707@gmail.com>
2017-10-22 16:20:14 +0300

    added async processing lab

---
+++ 03.AsynchronousProcessingLab/01.ImageFlipper/ImageFlipper.cs
@@ -0,0 +1,44 @@
+﻿namespace _01.ImageFlipper
+{ 
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class ImageFlipper
+    {
+        public static void Main()
+        {
+            string currDirectory = Directory.GetCurrentDirectory();
+            var directoryInfo = new DirectoryInfo(currDirectory + "\\Images");
+
+            const string resultDirectory = "Result";
+            if (Directory.Exists(resultDirectory))
+            {
+                Directory.Delete(resultDirectory, true);
+            }
+
+            Directory.CreateDirectory(resultDirectory);
+
+            List<Task> tasks = new List<Task>();
+            var files = directoryInfo.GetFiles();
+            foreach (var file in files)
+            {
+                var task = Task.Run(() =>
+                {
+                    var image = Image.FromFile(file.FullName);
+                    image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                    image.Save($"{resultDirectory}\\flip-{file.Name}");
+
+                    Console.WriteLine($"{file.Name} processed...");
+                });
+
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("--Finished--");
+        }
+    }
+}
---
+++ 03.AsynchronousProcessingLab/02.WebServerWithTcpListener/WebServerWithTcpListener.cs
@@ -0,0 +1,43 @@
+﻿namespace _02.WebServerWithTcpListener
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class WebServerWithTcpListener
+    {
+        public static void Main()
+        {
+            int port = 1337;
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var tcpListener = new TcpListener(ipAddress, port);
+
+            tcpListener.Start();
+
+            Task
+                .Run(async () => await Connect(tcpListener))
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        public static async Task Connect(TcpListener listener)
+        {
+            while (true)
+            {
+                using (var client = await listener.AcceptTcpClientAsync())
+                {
+                    var buffer = new byte[1024];
+                    await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+                    var clientMessage = Encoding.UTF8.GetString(buffer);
+                    Console.WriteLine(clientMessage.Trim());
+
+                    var responseMessage = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHi!!!";
+                    var data = Encoding.UTF8.GetBytes(responseMessage);
+                    await client.GetStream().WriteAsync(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
